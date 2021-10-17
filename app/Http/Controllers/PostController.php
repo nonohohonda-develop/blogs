2709b5e41eed5a381e676474ffee22060f1214b7
nonohohonda-develop <nnhhnd3012@gmail.com>
2021-10-17 16:18:09 +0900

    #6 新規投稿ページ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,4 +9,8 @@ class PostController extends Controller
    public function index(){
        return view('posts.index');
    }
+
+   public function create(){
+       return view('posts.create');
+   }
 }
