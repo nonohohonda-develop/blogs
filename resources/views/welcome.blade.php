3b0807896e5016fb9dbf87e0c253aca1f7103c36
nonohohonda-develop <nnhhnd3012@gmail.com>
2021-10-17 13:58:54 +0900

    #3 タイトル変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TEST BLOGS
                 </div>
 
                 <div class="links">
